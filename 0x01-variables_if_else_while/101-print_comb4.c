a59767214bf069b3e258a7dc3be026e280305134
IgbaoretoW <wigbaoreto@gmail.com>
2023-06-20 14:08:30 -0700

    combination of two numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,12 +8,12 @@
 int main(void)
 {
 	int a, b, c;
-	
+
 	for (a = 48; a <= 56; a++)
 	{
 		for (b = 49; b <= 57; b++)
 		{
-			for (c = 50: c <= 58; c++)
+			for (c = 50; c <= 58; c++)
 			{
 				if (c > b && b > a)
 				{
