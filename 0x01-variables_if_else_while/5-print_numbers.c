619d82f133b4e03d3ce6ac9f975515b3e0666353
IgbaoretoW <wigbaoreto@gmail.com>
2023-06-19 17:23:20 -0700

    print single numbers between 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - print single numbers from 0 to 10
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0, i < 10, i++)
+	{
+	printf("%d", i);
+	}
+	putchar("\n");
+	return (0);
+}
