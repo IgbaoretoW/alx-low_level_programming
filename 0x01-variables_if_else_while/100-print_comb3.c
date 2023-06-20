8dc9fe5cf1bb20fa82a9aca5d07882eb311a8840
IgbaoretoW <wigbaoreto@gmail.com>
2023-06-19 19:43:17 -0700

    2 combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+/**
+ * main - prints all possible combinations of 2 numbers.
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i, f;
+	
+	for (i = 48; i < 56; i++)
+	{
+		for (f = 49; f < 57; f++)
+	{
+		putchar(i);
+		if (f > i)
+		{
+		putchar ('i');
+		putchar ('f');
+			if (i != 56 || f != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
