0b75b9155cc0d07114fb9a124aedb7d6450b4a40
IgbaoretoW <wigbaoreto@gmail.com>
2023-06-19 18:57:08 -0700

    print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
 /**
- * main - print single digit number between 0 to 9
+ * main - prints single digit number between 0 to 9
  *
  * Return: Always 0 (Success)
 */
 int main(void)
 {
-	char numberz[10] = "0123456789"
+	char numberz[10] = "0123456789";
 	int i;
 
-	for (i = 0, i < 10, i++)
+	for (i = 0; i < 10; i++)
 	{
-		putchar(numberz[10]
+		putchar(numberz[10]);
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
