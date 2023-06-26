1d077c4208edec2f7cded97fa70f8aac294b0508
IgbaoretoW <wigbaoreto@gmail.com>
2023-06-26 15:48:55 -0700

    pointers arrays string

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _atoi - converts a string to an integer
- *
+ * @s - string converted
  * Return: the int converted from the string
  */
 int _atoi(char *s)
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,8 @@
 include "main.h"
 /**
  * print_array - a function that prints n elements of an array
- *
+ * @a: array name
+ * @n: number of elements OF the array to be printed
  * Return: a and n inputs
  */
 void print_array(int *a, int n)
